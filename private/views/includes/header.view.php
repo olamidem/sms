e746cfb8e3a33974a4791fc31cc7a9fb656ef3bd
OLAMIDE OLAGUNJU <olamxii@gmail.com>
2023-08-01 08:57:30 +0100

    Update header.view.php

--- private/views/includes/header.view.php
+++ private/views/includes/header.view.php
@@ -7,6 +7,8 @@
     <meta name="description" content="school management system">
     <meta name="keywords" content="HTML, CSS, PHP, Javascript">
     <link rel="stylesheet" href="../public/src/output.css">
+    <script src="//unpkg.com/alpinejs" defer></script>
+
     <title>Home view</title>
     <script></script>
 </head>
