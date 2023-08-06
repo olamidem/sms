f66855b9ae0adecaf9fb39ecde24f768be93befa
OLAMIDE OLAGUNJU <olamxii@gmail.com>
2023-08-06 11:50:18 +0100

    add switch school button to the school view

--- private/views/schools.view.php
+++ private/views/schools.view.php
@@ -105,6 +105,18 @@
                                                     </svg>
                                                 </a>
                                             </div>
+                                            <div class="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
+                                                <a href="<?= ROOT ?>/switch_school/<?= $row->id ?>" class="">
+                                                    <svg xmlns="http://www.w3.org/2000/svg"
+                                                        class="h-4 w-6 text-gray-600 font-extrabold" fill="none"
+                                                        viewBox="0 0 24 24" stroke="currentColor">
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                            d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                            d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                                    </svg>
+                                                </a>
+                                            </div>
                                         </div>
                                     </td>
                                 </tr>
